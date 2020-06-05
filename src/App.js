0f480e3c56ae6fb977d7544414a42126c551446d
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-05 17:01:06 +0200

    weather object

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import Location from "./location";
 import Weather from "./weather";
+import axios from "axios";
 
 function App() {
   const [geoLocation, setGeoLocation] = useState("...connecting");
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [weatherTheme, setWeatherTheme] = useState(null);
+  const [weatherDescript, setWeatherDescript] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     function showPosition() {
@@ -19,6 +23,7 @@ function App() {
           );
           setLatitude(position.coords.latitude);
           setLongitude(position.coords.longitude);
+          console.log("location data received");
         });
       } else {
         setGeoLocation(
@@ -26,8 +31,36 @@ function App() {
         );
       }
     }
+
     showPosition();
   });
+
+  function showWeather() {
+    if (!latitude || !longitude) {
+      return console.log("waiting for location data");
+    }
+    axios({
+      method: "get",
+      url: "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139",
+      params: {
+        lat: latitude,
+        lon: longitude,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        setWeather(response.data.main);
+        setWeatherTheme(response.data.weather[0].main);
+        setWeatherDescript(response.data.weather[0].description);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    showWeather();
+  }, [latitude, longitude]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -38,7 +71,14 @@ function App() {
             <p>Local Weather Report</p>
           </header>
           <Location geoLocation={geoLocation} />
-          <Weather setWeather={setWeather} lon={longitude} lat={latitude} />
+          <Weather
+            setWeatherDescript={setWeatherDescript}
+            setWeatherTheme={setWeatherTheme}
+            lon={longitude}
+            lat={latitude}
+          />
+          Weather Theme: {weatherTheme}
+          Weather Description: {weatherDescript}
           <footer className="App-footer">Helen Maher</footer>
         </div>
       </>
--- src/weather.js
+++ src/weather.js
@@ -1,35 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import axios from "axios";
 
 export const Weather = (props) => {
-  const { lon, lat } = props;
-  const [weather, setWeather] = useState(null);
+  const { lon, lat, setWeatherTheme, setWeatherDescript } = props;
 
-  function showWeather() {
-    axios({
-      method: "get",
-      url: "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139",
-      params: {
-        lat: lat,
-        lon: lon,
-      },
-    })
-      .then((response) => {
-        console.log(response);
-        setWeather(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  return <div>Weather: {weather}</div>;
+  return <div className="weather">Weather:</div>;
 };
 
 export default Weather;
 
 Weather.propTypes = {
-  lon: PropTypes.string,
-  lat: PropTypes.string,
+  setWeatherTheme: PropTypes.func.isRequired,
+  setWeatherDescript: PropTypes.func.isRequired,
+  lon: PropTypes.number,
+  lat: PropTypes.number,
 };
