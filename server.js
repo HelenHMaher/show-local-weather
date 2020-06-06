a92746b4862e3b8f02b4bf295bf060c02d1b0d23
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-06 14:38:37 +0200

    legend of aaron

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// use the express-static middleware
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/heartbeat", function (req, res) {
-  res.send("<3");
-});
-// define the first route
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+// use the express-static middleware
+// app.use(express.static(__dirname));
+app.use("/show-local-weather/", express.static(path.join(__dirname, "build")));
+// app.get("/heartbeat", function (req, res) {
+//   res.send("<3");
+// });
+// define the first route
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
