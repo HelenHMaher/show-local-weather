7cd9a8357ca73148913f3568390257a31b3350e2
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-06 10:49:08 +0200

    trying to adjust routing to serve on heroku

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // create an express app
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // use the express-static middleware
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "build")));
 
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
+app.get("/*", function (req, res) {
+  res.send(path.join(__dirname, "build", "index.html"));
 });
 
 // start the server listening for requests
