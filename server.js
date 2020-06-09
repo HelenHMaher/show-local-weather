68697714ac5d525fd54f2628dd5b21c8c017f9a1
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-09 09:54:54 +0200

    favicon dependencies

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-// create an express app
 const express = require("express");
+const favicon = require("express-favicon");
 const path = require("path");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-// use the express-static middleware
-// app.use(express.static(__dirname));
+app.use(favicon(__dirname + "/build/favicon.png"));
 app.use("/show-local-weather/", express.static(path.join(__dirname, "build")));
 app.get("/heartbeat", function (req, res) {
   res.send("<3");
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // start the server listening for requests
