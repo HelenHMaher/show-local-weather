911fd88aa311a66d9bd578c73b2a2f053e1ce096
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-09 09:30:24 +0200

    json format parameter

--- server.js
+++ server.js
@@ -10,13 +10,9 @@ app.get("/", function (req, res) {
 // use the express-static middleware
 // app.use(express.static(__dirname));
 app.use("/show-local-weather/", express.static(path.join(__dirname, "build")));
-// app.get("/heartbeat", function (req, res) {
-//   res.send("<3");
-// });
-// define the first route
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.get("/heartbeat", function (req, res) {
+  res.send("<3");
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
