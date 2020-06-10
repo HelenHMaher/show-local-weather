041de474e515ec62e59f1ea36c5d5b2565aec367
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-10 23:05:18 +0200

    cors still a problem

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ const favicon = require("express-favicon");
 const path = require("path");
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    process.env.STATUS === "development"
+      ? "http://localhost:3000/show-local-weather/"
+      : "https://local-weather-hm.herokuapp.com/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(favicon(__dirname + "/build/favicon.png"));
 app.use("/show-local-weather/", express.static(path.join(__dirname, "build")));
 app.get("/heartbeat", function (req, res) {
   res.send("<3");
 });
 
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
