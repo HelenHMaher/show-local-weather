4769a708862f1852bcb93e88b1c5dbdbbe87bc41
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-12 09:59:26 +0200

    get location data

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React, { useState, useEffect } from "react";
 import { GlobalStyles } from "./global";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import Location from "./Components/location";
-import Weather from "./Components/weather";
+import Location from "./Components/location/location";
+import Weather from "./Components/weather/weather";
 import axios from "axios";
 
 function App() {
-  const [getMyLocation, setMyLocation] = useState(false);
+  const [getMyLocation, setMyLocation] = useState(true);
+  const [haveMyLocation, setHaveMyLocation] = useState(false);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [weatherTheme, setWeatherTheme] = useState(null);
+
   const [weatherDescript, setWeatherDescript] = useState(null);
   const [temp, setTemp] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [pressure, setPressure] = useState(null);
   const [windSpeed, setWindSpeed] = useState(null);
   const [cloudCover, setCloudCover] = useState(null);
-  const [city, setCity] = useState(null);
-  const [country, setCountry] = useState(null);
 
-  function handleSubmit(city, country) {
-    setCity(city);
-    setCountry(country);
-    getCoords();
+  //getLatandLon from Location
+  function submitLatLon(input) {
+    setLatitude(input.geometry.coordinates[1]);
+    setLongitude(input.geometry.coordinates[0]);
   }
 
+  //toggle getMyLocation from Location
   function clickHandler() {
     setMyLocation(!getMyLocation);
+    setHaveMyLocation(false);
   }
 
   function showPosition() {
@@ -37,60 +39,17 @@ function App() {
           lat: position.coords.latitude,
           lon: position.coords.longitude,
         };
-        //console.log(result);
         setLatitude(result.lat);
         setLongitude(result.lon);
-        showCity(result);
-        showWeather(result);
+        console.log("location data");
+        setHaveMyLocation(true);
       });
     } else {
       alert("Sorry, your browser does not support HTML5 geolocation.");
     }
   }
 
-  function getCoords(country, city) {
-    axios({
-      method: "get",
-      url:
-        "https://cors-anywhere.herokuapp.com/https://nominatim.openstreetmap.org/search",
-      params: {
-        country: country,
-        city: city,
-        format: "geojson",
-      },
-    })
-      .then((response) => {
-        console.log(response);
-        console.log("geolocation");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  function showCity(coordinates) {
-    axios({
-      method: "get",
-      url:
-        "https://cors-anywhere.herokuapp.com/https://nominatim.openstreetmap.org/reverse",
-      params: {
-        lat: coordinates.lat,
-        lon: coordinates.lon,
-        format: "json",
-        zoom: "14",
-      },
-    })
-      .then((response) => {
-        setCity(response.data.address.borough);
-        setCountry(response.data.address.country);
-        //console.log(response);
-        console.log("reverseGeolocation");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
+  /*
   function showWeather(coordinates) {
     axios({
       method: "get",
@@ -116,12 +75,13 @@ function App() {
         console.log(error);
       });
   }
+*/
 
   useEffect(() => {
     if (getMyLocation) {
       showPosition();
     }
-  }, []);
+  }, [getMyLocation]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -135,10 +95,9 @@ function App() {
             getMyLocation={getMyLocation}
             lat={latitude}
             lon={longitude}
-            city={city}
-            country={country}
             clickHandler={clickHandler}
-            handleSubmit={handleSubmit}
+            submitLatLon={submitLatLon}
+            haveMyLocation={haveMyLocation}
           />
           <Weather
             temp={temp}
---
+++ src/Components/location/location.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+
+export const Location = (props) => {
+  const {
+    lat,
+    lon,
+    getMyLocation,
+    clickHandler,
+    submitLatLon,
+    haveMyLocation,
+  } = props;
+
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [tempCity, setTempCity] = useState("");
+  const [tempCountry, setTempCountry] = useState("");
+  const [placeName, setPlaceName] = useState("");
+
+  function showCity() {
+    axios({
+      method: "get",
+      url:
+        "https://cors-anywhere.herokuapp.com/https://nominatim.openstreetmap.org/reverse",
+      params: {
+        lat: lat,
+        lon: lon,
+        format: "json",
+        zoom: "14",
+      },
+    })
+      .then((response) => {
+        setCity(response.data.address.borough);
+        setCountry(response.data.address.country);
+        //console.log(response);
+        console.log("reverse Geolocation");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    axios({
+      method: "get",
+      url:
+        "https://cors-anywhere.herokuapp.com/https://nominatim.openstreetmap.org/search",
+      params: {
+        country: tempCountry,
+        city: tempCity,
+        format: "geojson",
+      },
+    })
+      .then((response) => {
+        console.log("geolocation");
+        submitLatLon(response.data.features[0]);
+        setCity(tempCity);
+        setCountry(tempCountry);
+        setTempCity("");
+        setTempCountry("");
+        setPlaceName(response.data.features[0].properties.display_name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (getMyLocation && haveMyLocation) {
+      showCity();
+    }
+  }, [getMyLocation, haveMyLocation]);
+
+  if (getMyLocation) {
+    return (
+      <div className="location">
+        <button className="getLocation" onClick={clickHandler}>
+          Choose a Location
+        </button>
+        <div className="latAndLon">
+          Latitude: {lat} Longitude: {lon}
+        </div>
+        <div className="city">
+          City: {city} Country: {country}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="location">
+        <button className="getLocation" onClick={clickHandler}>
+          Get my Location
+        </button>
+        <div className="latAndLon">
+          Latitude: {lat} Longitude: {lon}
+        </div>
+        <form className="getLocationForm" onSubmit={handleSubmit}>
+          <label htmlFor="cityInput">City: </label>
+          <input
+            value={tempCity}
+            name="cityInput"
+            id="cityInput"
+            placeholder="city"
+            required
+            onChange={(e) => setTempCity(e.target.value)}
+          />
+          <label htmlFor="countryInput">Country: </label>
+          <input
+            value={tempCountry}
+            name="countryInput"
+            id="countryInput"
+            placeholder="country"
+            required
+            onChange={(e) => setTempCountry(e.target.value)}
+          />
+          <input type="submit" />
+        </form>
+        <div className="displayLocation">
+          <h1>{city}</h1> <h2>{country}</h2>
+          <h5>{placeName}</h5>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Location;
+
+Location.propTypes = {
+  submitLatLon: PropTypes.func,
+  getMyLocation: PropTypes.bool,
+  clickHandler: PropTypes.func,
+  lat: PropTypes.number,
+  lon: PropTypes.number,
+  haveMyLocation: PropTypes.bool,
+};
